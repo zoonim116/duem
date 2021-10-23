51a1d590a067f9541035633c9a96f4718cca9a02
Maxim Nepomniashchiy <maxim@modzi.team>
2021-10-23 19:09:03 +0300

    Add social links to footer

--- duem/footer.php
+++ duem/footer.php
@@ -1,25 +1,31 @@
 <footer>
-      <div class="container">
+    <div class="container">
         <div class="row justify-content-between align-content-center">
-          <div class="col logo-wrap">
-            <a href="/">Duem</a>
-          </div>
-          <div class="col footer-social">
-            <ul class="d-flex justify-content-end">
-              <li><a href="#" target="_blank">
-                <img src="<?php echo get_template_directory_uri()?>/assets/img/facebook.svg">
-              </a></li>
-              <li><a href="#" target="_blank">
-                <img src="<?php echo get_template_directory_uri()?>/assets/img/insta.svg">
-              </a></li>
-              <li><a href="#" target="_blank">
-                <img src="<?php echo get_template_directory_uri()?>/assets/img/telegram.svg">
-              </a></li>
-            </ul>
-          </div>
+            <div class="col logo-wrap">
+                <a href="<?php echo get_site_url(); ?>">Duem</a>
+            </div>
+            <div class="col footer-social">
+                <ul class="d-flex justify-content-end">
+					<?php if ( get_field( 'facebook', 'option' ) ): ?>
+                        <li><a href="<?php the_field( 'facebook', 'option' ); ?>" target="_blank">
+                                <img src="<?php echo get_template_directory_uri() ?>/assets/img/facebook.svg">
+                            </a></li>
+					<?php endif; ?>
+					<?php if ( get_field( 'instagram', 'option' ) ): ?>
+                        <li><a href="<?php the_field( 'instagram', 'option' ); ?>" target="_blank">
+                                <img src="<?php echo get_template_directory_uri() ?>/assets/img/insta.svg">
+                            </a></li>
+					<?php endif; ?>
+					<?php if ( get_field( 'telegram', 'option' ) ): ?>
+                        <li><a href="<?php the_field('telegram', 'option'); ?>" target="_blank">
+                                <img src="<?php echo get_template_directory_uri() ?>/assets/img/telegram.svg">
+                            </a></li>
+					<?php endif; ?>
+                </ul>
+            </div>
         </div>
-      </div>
-    </footer>
-    <?php wp_footer(); ?>
-  </body>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
